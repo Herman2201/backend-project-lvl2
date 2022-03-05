36ff7fb2d2d86a1566dd7545bd72529aa55aea07
Herman2201 <84591802+Herman2201@users.noreply.github.com>
2022-03-06 00:53:07 +0200

    fix file

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import parseFileData from './src/parsers.js';
 import diff from './src/diff.js';
 import treeToString from './src/treeToString.js';
 
-
 const genDiff = (file1, file2) => {
   const dataFile1 = parseFileData(file1);
   const dataFile2 = parseFileData(file2);
