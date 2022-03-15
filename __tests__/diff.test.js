52782d84908480e6a490be41c5e28c0566d51f56
Herman2201 <84591802+Herman2201@users.noreply.github.com>
2022-03-16 01:34:15 +0200

    add new test

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,29 +1,92 @@
+import path from 'path';
+import { readFileSync } from 'fs';
 import { test, expect } from '@jest/globals';
 import genDiff from '../index.js';
-import { readFile } from '../src/utils.js';
 
 const fileJson1 = 'file1.json';
 const fileJson2 = 'file2.json';
-const fileJson3 = 'file3.jso';
-const fileJson4 = 'file4.jso';
 const fileYaml1 = 'file1.yaml';
 const fileYaml2 = 'file2.yaml';
 const fileYml1 = 'file1.yml';
 const fileYml2 = 'file2.yml';
-const expecti = readFile('expect.txt');
+const expectStylish = readFileSync(
+  path.join(path.resolve(), '__fixtures__', 'expectStylish.txt'),
+  'utf8',
+);
+const expectPlain = readFileSync(
+  path.join(path.resolve(), '__fixtures__', 'expectPlain.txt'),
+  'utf8',
+);
+const expectJSON = readFileSync(
+  path.join(path.resolve(), '__fixtures__', 'expectJSON.txt'),
+  'utf8',
+);
+const styleStylish = 'stylish';
+const stylePlain = 'plain';
+const styleJSON = 'json';
 
-test('diff file1.json, file2.json', () => {
-  expect(genDiff(fileJson1, fileJson2)).toBe(expecti);
+describe('Bad test', () => {
+  test.each([
+    {
+      badExtname1: 'file1.jso',
+      badExtname2: 'file2.json',
+    },
+    {
+      badExtname1: 'file1.yam',
+      badExtname2: 'file2.yaml',
+    },
+    {
+      badExtname1: 'file1.ym',
+      badExtname2: 'file2.yml',
+    },
+    {
+      badExtname1: 'file1',
+      badExtname2: 'file2.yml',
+    },
+  ])('error extname', ({ badExtname1, badExtname2 }) => {
+    expect(() => genDiff(badExtname1, badExtname2)).toThrow();
+  });
+  test.each([
+    {
+      file1: fileJson1,
+      file2: fileJson2,
+      badFormat: 'stylih',
+    },
+    {
+      file1: fileYaml1,
+      file2: fileYaml2,
+      badFormat: 'Plain',
+    },
+    {
+      file1: fileYml1,
+      file2: fileYml2,
+      badFormat: 'jSon',
+    },
+  ])('error format: stulih, Plain, jSon', ({ file1, file2, badFormat }) => {
+    expect(() => genDiff(file1, file2, badFormat)).toThrow();
+  });
 });
+describe('diff test extname: json, yaml, yml. Format: stylish, plain, json', () => {
+  test.each([
+    { file1: fileJson1, file2: fileJson2, formatName: styleStylish },
+    { file1: fileYaml1, file2: fileYaml2, formatName: styleStylish },
+    { file1: fileYml1, file2: fileYml2, formatName: styleStylish },
+  ])('Format stulihs', ({ file1, file2, formatName }) => {
+    expect(genDiff(file1, file2, formatName)).toBe(expectStylish);
+  });
 
-test('diff file3.yaml, file4.yaml', () => {
-  expect(genDiff(fileYaml1, fileYaml2)).toBe(expecti);
-});
-
-test('diff file1.yml, file1.yml', () => {
-  expect(genDiff(fileYml1, fileYml2)).toBe(expecti);
-});
-
-test('throw error', () => {
-  expect(() => genDiff(fileJson3, fileJson4)).toThrow();
+  test.each([
+    { file1: fileJson1, file2: fileJson2, formatName: stylePlain },
+    { file1: fileYaml1, file2: fileYaml2, formatName: stylePlain },
+    { file1: fileYml1, file2: fileYml2, formatName: stylePlain },
+  ])('Format plain', ({ file1, file2, formatName }) => {
+    expect(genDiff(file1, file2, formatName)).toBe(expectPlain);
+  });
+  test.each([
+    { file1: fileJson1, file2: fileJson2, formatName: styleJSON },
+    { file1: fileYaml1, file2: fileYaml2, formatName: styleJSON },
+    { file1: fileYml1, file2: fileYml2, formatName: styleJSON },
+  ])('Format json', ({ file1, file2, formatName }) => {
+    expect(genDiff(file1, file2, formatName)).toBe(expectJSON);
+  });
 });
