62c2a90175ae854996a8a7ef4ea539715820710f
Herman2201 <84591802+Herman2201@users.noreply.github.com>
2022-03-16 01:34:53 +0200

    fix and delete file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,9 +8,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => {
-    const result = genDiff(filepath1, filepath2);
-    console.log(result);
+  .option('-f, --format <type>', 'output format [stylish, plain, json]', 'stylish')
+  .action((filepath1, filepath2, formatName) => {
+    const diff = genDiff(filepath1, filepath2, formatName.format);
+    console.log(diff);
   })
   .parse(process.argv);
